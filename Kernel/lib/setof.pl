7c65e0dbad8def0c4d054f6ab057fc5477e0c478
jschimpf <jschimpf>
2013-01-23 22:33:03 +0000

    Revision of bagof/3 to fix known bugs in O'Keefe's original code, to deal correctly with attributed variables, and to support ISO semantics.

--- Kernel/lib/setof.pl
+++ Kernel/lib/setof.pl
@@ -20,67 +20,21 @@
 % Contributor(s): R.A.O'Keefe and David Warren
 % Contributor(s): ECRC GmbH
 % Contributor(s): IC-Parc, Imperal College London
+% Contributor(s): Joachim Schimpf, Coninfer Ltd
 % 
 % END LICENSE BLOCK
-%
-% System:	ECLiPSe Constraint Logic Programming System
-% Version:	$Id: setof.pl,v 1.3 2009/07/16 09:11:24 jschimpf Exp $
 % ----------------------------------------------------------------------
-
-/*
- * SEPIA PROLOG SOURCE MODULE
- */
-
-/*
- * IDENTIFICATION:	setof.pl
- *
- * DESCRIPTION: 	Implements setof features
- *			See comments below.
- *
- * CONTENTS:     
- *
- */
-
-%   File   : SETOF.PL
-%   Author : R.A.O'Keefe
-%   Updated: 17 November 1983
-%   Purpose: define setof/3, bagof/3, findall/3, and findall/4
-%   Needs  : Not.Pl
-%   Updated: 21 October 1987 by micha, included a part of not.pl
-%   Updated: 21 Feb 1990 by joachim, using erase_list/3 and occurs/2
-%   Updated: 25 May 1990 by micha, made it invisible to the debugger
-%   Updated: 2 Nov 1992 by joachim, made nodbgcomp, added useful modes
-%   Updated: 14 Oct 1994 by joachim, modified to use bag_enter/2 etc
-
-/*  This file defines two predicates which act like setof/3 and bagof/3.
-    I have seen the code for these routines in Dec-10 and in C-Prolog,
-    but I no longer recall it, and this code was independently derived
-    in 1982 by me and me alone.
-
-    Most of the complication comes from trying to cope with free variables
-    in the Filter; these definitions actually enumerate all the solutions,
-    then group together those with the same bindings for the free variables.
-    There must be a better way of doing this.  I do not claim any virtue for
-    this code other than the virtue of working.  In fact there is a subtle
-    bug: if setof/bagof occurs as a data structure in the Generator it will
-    be mistaken for a call, and free variables treated wrongly.  Given the
-    current nature of Prolog, there is no way of telling a call from a data
-    structure, and since nested calls are FAR more likely than use as a
-    data structure, we just put up with the latter being wrong.  The same
-    applies to negation.
-
-    Would anyone incorporating this in their Prolog system please credit
-    both me and David Warren;  he thought up the definitions, and my
-    implementation may owe more to subconscious memory of his than I like
-    to think.  At least this ought to put a stop to fraudulent claims to
-    having bagof, by replacing them with genuine claims.
-
-    Thanks to Dave Bowen for pointing out an amazingly obscure bug: if
-    the Template was a variable and the Generator never bound it at all
-    you got a very strange answer!  Now fixed, at a price.
-*/
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% System:		ECLiPSe Constraint Logic Programming System
+% Version:		$Id: setof.pl,v 1.4 2013/01/23 22:33:03 jschimpf Exp $
+% Identification:	setof.pl
+% Description:		Implements the all-solution predicates.
+% 			This code was originally based on Richard O'Keefe's
+% 			1983 public domain implementation, but now retains
+% 			virtually nothing from that base, except the
+% 			wording of some comments.
+%
+% ----------------------------------------------------------------------
 
 :- system.
 
@@ -91,7 +45,6 @@
 	bagof/3,
 	(^)/2.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %   findall(Template, Generator, List)
 %   is a special case of bagof, where all free variables in the
@@ -107,7 +60,7 @@ findall_body(Template, Generator, List, Module) :-
 
 
 %   setof(Template, Generator, Set)
-%   finds the Set of instances of the Template satisfying the Generator..
+%   finds the Set of instances of the Template satisfying the Generator.
 %   The set is in ascending order (see compare/3 for a definition of
 %   this order) without duplicates, and is non-empty.  If there are
 %   no solutions, setof fails.  setof may succeed more than one way,
@@ -119,14 +72,13 @@ setof_body(Template, Filter, Set, Module) :-
 	sort(0, <, Bag, Set).
 
 
-%   coverof(Template, Generator, Set)
+%   coverof(Template, Generator, Set) - ECLiPSe extension
 %   works like setof/3, however the list of solutions is not sorted
 %   and only the most general instances are retained
 
 coverof_body(Template, Filter, Set, Module) :-
 	bagof_body(Template, Filter, Bag, Module),
 	prune_instances(Bag, Set).
-
 
 
 %   bagof(Template, Generator, Bag)
@@ -136,33 +88,138 @@ coverof_body(Template, Filter, Set, Module) :-
 %   Template, it assumes that this is like any other Prolog question
 %   and that you want bindings for those variables.  (You can tell it
 %   not to bother by using existential quantifiers.)
-%   bagof records two things:
-%	terms with no free variables   -Term
-%	terms with free variables   Key-Term
-%   The second clause is basically just findall, which of course works in
-%   the common case when there are no free variables.
 
-bagof_body(Template, Generator, Bag, Module) :-
-	free_variables(Generator, Template, [], Vars),
-	Vars \== [],
-	!,
-	Key =.. [.|Vars],
-	functor(Key, ., N),
-	bag_create(Ref),
-	recordz_instances(Key-Template, Generator, Module, Ref),
-	collect_instances(Key, N, Ref, Bag).
-bagof_body(Template, Generator, Bag, Module) :-
-	bag_create(Ref),
-	recordz_instances(Template, Generator, Module, Ref),
-	bag_dissolve(Ref, Bag),
-	Bag \== [].
+bagof_body(Template, QGenerator, Bag, Module) :-
+	free_variables_quant(QGenerator, Template, Generator, Vars, Module),
+	( Vars == [] ->
+	    bag_create(Ref),
+	    recordz_instances(Template, Generator, Module, Ref),
+	    bag_dissolve(Ref, Bag),
+	    Bag \== []
+	;
+	    Key =.. [.|Vars],
+	    bag_create(Ref),
+	    recordz_instances(Key-Template, Generator, Module, Ref),
+	    bag_dissolve(Ref, KeyedSols),
+	    add_stripped_keys(KeyedSols, DoubleKeyedSols, HaveAttributes),
+	    ( HaveAttributes == true ->
+		instance_bag_attrs(DoubleKeyedSols, Key, Bag)
+	    ;
+		instance_bag_plain(KeyedSols, Key, Bag)
+	    )
+	).
 
-:- mode collect_instances(+,++,++,?).
-collect_instances(Key, N, Ref, Bag) :-
-	list_instances(Key, N, Ref, OmniumGatherum),
-	keysort(OmniumGatherum, Gamut),
-	concordant_subset(Gamut, Key, Answer),
-	Bag = Answer.
+
+% The simpler form for "plain" solutions, which have no attributes in their
+% free variable bindings, and thus are cheaper to partition into variants.
+
+instance_bag_plain(KeyedSols, FreeVars, Bag) :-
+	% Unify variables in FreeBindings. As a result, variants become
+	% identical (the sharing side-effect among non-variants is unimportant).
+	% The trick was suggested by Ulrich Neumerkel.
+	equalise_key_variables(KeyedSols, _),
+	sort(1, =<, KeyedSols, SortedKeyedSols),
+	same_key_members(SortedKeyedSols, FreeVars, Bag).
+
+
+% The general form, where free variable bindings can be attributed vars.
+% We have a list of FreeBindingsPlain-(FreeBindingsAttr-TemplateBinding)
+% We first use FreeBindingsPlain to group solutions as in the attribute-free
+% case.  Within these groups we then use the naive algorithm for further
+% partitioning using full attribute-aware variant testing on FreeBindingsAttr.
+
+instance_bag_attrs(DoubleKeyedSols, FreeVars, Bag) :-
+	equalise_key_variables(DoubleKeyedSols, _),
+	sort(1, =<, DoubleKeyedSols, SortedDoubleKeyedSols),
+	same_key_members(SortedDoubleKeyedSols, _, MultiBag),
+	instance_bag_naive(MultiBag, FreeVars, Bag).
+
+
+% Naive version with full variant testing, quadratic complexity
+instance_bag_naive([FreeBinding1-TmplBinding1|Sols], FreeVars, Bag) :-
+	(
+	    foreach(Sol,Sols),
+	    fromto(RemSols,RemSols1,RemSols2,[]),
+	    fromto(Bag1,Bag2,Bag3,[]),
+	    param(FreeBinding1)
+	do
+	    Sol = FreeBinding-TmplBinding,
+	    ( variant(FreeBinding, FreeBinding1) ->
+		FreeBinding1 = FreeBinding,
+		Bag2 = [TmplBinding|Bag3],
+		RemSols1 = RemSols2
+	    ;
+		Bag2 = Bag3,
+		RemSols1 = [Sol|RemSols2]
+	    )
+	),
+	( RemSols == [] ->
+	    FreeVars = FreeBinding1, Bag = [TmplBinding1|Bag1]
+	;
+	    (
+		FreeVars = FreeBinding1, Bag = [TmplBinding1|Bag1]
+	    ;
+		instance_bag_naive(RemSols, FreeVars, Bag)
+	    )
+	).
+
+
+% Auxiliary operations on Key-Value lists
+
+% Add keys that are copies of the original keys with their attributes stripped.
+
+add_stripped_keys(KVs, KKVs, HaveAttributes) :-
+	(
+	    foreach(KV,KVs),
+	    foreach(PlainKey-KV,KKVs),
+	    param(HaveAttributes)
+	do
+	    KV = Key-_,
+	    copy_term(Key, PlainKey, AttrVars),
+	    ( AttrVars == [] -> true ; HaveAttributes = true )
+	).
+
+
+% Unify variables in the list keys.
+% This is similar to using numbervars, but leaves the variables free.
+
+equalise_key_variables([], _PositionVars).
+equalise_key_variables([Key-_|KVs], PositionVars) :-
+	term_variables(Key, FreeVars),
+	append(FreeVars, _, PositionVars),
+	equalise_key_variables(KVs, PositionVars).
+
+
+% Separate the leading list elements with identical keys
+
+same_key_prefix([Key-V|KVs], Key, [V|Vs], KVsRest) :-
+	same_key_prefix1(KVs, Key, Vs, KVsRest).
+
+    same_key_prefix1([], _Key, [], []).
+    same_key_prefix1(KVs, Key, Vs, KVsRest) :- KVs = [K-V|KVs1],
+    	( K == Key ->
+	    Vs = [V|Vs1],
+	    same_key_prefix1(KVs1, Key, Vs1, KVsRest)
+	;
+	    Vs = [], KVsRest = KVs
+	).
+    	
+	
+% Succeed once for each sequence of elements with identical keys
+
+same_key_members(KVs, Key, Vs) :-
+	% Pick the first sequence
+	same_key_prefix(KVs, Key0, Vs0, KVsRest),
+	% Succeed, and if necessary, allow backtracking for more
+	( KVsRest == [] ->
+	    Key = Key0, Vs = Vs0
+	;
+	    (
+		Key = Key0, Vs = Vs0
+	    ;
+		same_key_members(KVsRest, Key, Vs)
+	    )
+	).
 
 
 %    recordz_instances(Template, Generator, Module, Ref)
@@ -178,119 +235,42 @@ recordz_instances(Template, Generator, Module, Ref) :-
 recordz_instances(_, _, _, _).
 
 
+% Collect the free variables in QGoal.  These are those that occur
+% neither in Bound nor are explicitly quantified via X^Y^...^Goal.
+% If ^/2 is defined as a predicate, we use traditional quantifier-semantics,
+% else ISO-semantics (where only toplevel occurrences of ^/2 are considered).
+% The (toplevel-)unquantified goal is returned as well.
 
-%   list_instances(Key, NVars, Ref, Bag)
-%   pulls all the Key-Template instances out of the data base.
-%   Note that asserting something into the data base and pulling it out
-%   again renames all the variables; to counteract this we use replace_
-%   key_variables to put the old variables back.  Fortunately if we
-%   bind X=Y, the newer variable will be bound to the older, and the
-%   original key variables are guaranteed to be older than the new ones.
-%   This replacement must be done @i<before> the keysort.
-
-
-list_instances(Key, NVars, Ref, Bag) :-
-	bag_dissolve(Ref, Bag),
-	replace_all_key_variables(Bag, Key, NVars).
-
-replace_all_key_variables([], _, _).
-replace_all_key_variables([NewKey-_Term|Templates], Key, NVars) :-
-	replace_key_variables(NVars, Key, NewKey),
-	replace_all_key_variables(Templates, Key, NVars).
-
-
-replace_key_variables(0, _, _) :- !.
-replace_key_variables(N, OldKey, NewKey) :-
-	arg(N, NewKey, Arg),
-	replace_key_variable(Arg, OldKey, N),
-	M is N-1,
-	replace_key_variables(M, OldKey, NewKey).
-
-replace_key_variable(Arg, OldKey, N) :-
-	free(Arg),
-	arg(N, OldKey, Arg).
-replace_key_variable(Arg, OldKey, N) :-
-	meta(Arg),
-	arg(N, OldKey, OldArg),
-	( variant(Arg, OldArg) ->
-	    meta_bind(Arg, OldArg)
-	;
-	    true
+free_variables_quant(QGoal, Bound, Goal, VarList, _Module) :- var(QGoal), !,
+	Goal = QGoal,
+	free_variables(QGoal, Bound, [], VarList, false).
+free_variables_quant(Vars^QGoal, Bound, Goal, VarList, Module) :- !,
+	free_variables_quant(QGoal, Vars^Bound, Goal, VarList, Module).
+free_variables_quant(Goal, Bound, Goal, VarList, Module) :-
+	( current_built_in((^)/2)@Module -> UseQuant=true ; UseQuant=false ),
+	free_variables(Goal, Bound, [], VarList, UseQuant).
+	
+free_variables(Term, Bound, Vars0, Vars, _) :- var(Term),
+	( occurs(Term, Bound) -> Vars = Vars0
+	; occurs(Term, Vars0) -> Vars = Vars0
+	; Vars = [Term|Vars0]
 	).
-replace_key_variable(Arg, _, _) :-
-	nonvar(Arg).
-
-%   concordant_subset([Key-Val list], Key, [Val list]).
-%   takes a list of Key-Val pairs which has been keysorted to bring
-%   all the identical keys together, and enumerates each different
-%   Key and the corresponding lists of values.
-
-concordant_subset([Key-Val|Rest], Clavis, Answer) :-
-	concordant_subset(Rest, Key, List, More),
-	concordant_subset(More, Key, [Val|List], Clavis, Answer).
-
-
-%   concordant_subset(Rest, Key, List, More)
-%   strips off all the Key-Val pairs from the from of Rest,
-%   putting the Val elements into List, and returning the
-%   left-over pairs, if any, as More.
-
-:- mode	concordant_subset(+,+,-,-).
-concordant_subset([Key-Val|Rest], Clavis, [Val|List], More) :-
-	Key == Clavis,
-	!,
-	concordant_subset(Rest, Clavis, List, More).
-concordant_subset(More, _, [], More).
-
-
-%   concordant_subset/5 tries the current subset, and if that
-%   doesn't work if backs up and tries the next subset.  The
-%   first clause is there to save a choice point when this is
-%   the last possible subset.
-
-:- mode	concordant_subset(+,+,+,+,-).
-concordant_subset([],   Key, Subset, Key, Subset) :- !.
-concordant_subset(_,    Key, Subset, Key, Subset).
-concordant_subset(More, _,   _,   Clavis, Answer) :-
-	concordant_subset(More, Clavis, Answer).
-
-
-%   In order to handle variables properly, we have to find all the 
-%   universally quantified variables in the Generator.  All variables
-%   as yet unbound are universally quantified, unless
-%	a)  they occur in the template
-%	b)  they are bound by X^P, setof, or bagof
-%   free_variables(Generator, Template, OldList, NewList)
-%   finds this set, using OldList as an accumulator.
-
-free_variables(Term, Bound, OldVarList, VarList) :-
-	var(Term),
-	!,
-	(
-	occurs(Term, Bound) ->
-		VarList = OldVarList
+free_variables(Term, _Bound, Vars0, Vars, _) :- atomic(Term),
+	Vars0 = Vars.
+free_variables(Term, Bound, Vars0, Vars, UseQuant) :- compound(Term),
+	( UseQuant==true, explicit_binding(Term, Bound, NewTerm, NewBound) ->
+	    free_variables(NewTerm, NewBound, Vars0, Vars, UseQuant)
 	;
-	occurs(Term, OldVarList) ->
-		VarList = OldVarList
-	;
-		VarList = [Term|OldVarList]
+	    (
+		foreacharg(Argument,Term),
+		fromto(Vars0,Vars1,Vars2,Vars),
+		param(Bound,UseQuant)
+	    do
+		free_variables(Argument, Bound, Vars1, Vars2, UseQuant)
+	    )
 	).
-free_variables(Term, Bound, OldList, NewList) :-
-	explicit_binding(Term, Bound, NewTerm, NewBound),
-	!,
-	free_variables(NewTerm, NewBound, OldList, NewList).
-free_variables(Term, Bound, OldList, NewList) :-
-	arity(Term, N),
-	free_variables(N, Term, Bound, OldList, NewList).
 
-:- mode	free_variables(+,+,+,+,-).
-free_variables(0, _Term, _Bound, VarList, VarList) :- !.
-free_variables(N, Term, Bound, OldList, NewList) :-
-	arg(N, Term, Argument),
-	free_variables(Argument, Bound, OldList, MidList),
-	M is N-1, !,
-	free_variables(M, Term, Bound, MidList, NewList).
-
+% Traditional, non-ISO feature:
 %   explicit_binding checks for goals known to existentially quantify
 %   one or more variables.  In particular \+ is quite common.
 
@@ -303,26 +283,32 @@ explicit_binding(setof(Var,Goal,Set),  Bound, Goal-Set, Bound+Var) :- !.
 explicit_binding(bagof(Var,Goal,Bag),  Bound, Goal-Bag, Bound+Var) :- !.
 explicit_binding(coverof(Var,Goal,Bag),  Bound, Goal-Bag, Bound+Var) :- !.
 
+
+% The tool body of ^/2
+
+exquant_body(_, Goal, Module) :-
+	untraced_call(Goal, Module).
+
+
 % For proper tracing behaviour, this file must be in nodbgcomp
 % and the metapredicates must be set to unskipped. This will cause
 % only the metacalls of the user goals to show up in the trace.
 
 :- unskipped
+	exquant_body/3,
 	setof_body/4,
 	bagof_body/4,
 	coverof_body/4,
 	findall_body/4.
 
+:- set_flag(setof/3, trace_meta, on).
 :- set_flag(setof_body/4, trace_meta, on).
+:- set_flag(bagof/3, trace_meta, on).
 :- set_flag(bagof_body/4, trace_meta, on).
+:- set_flag(coverof/3, trace_meta, on).
 :- set_flag(coverof_body/4, trace_meta, on).
+:- set_flag(findall/3, trace_meta, on).
 :- set_flag(findall_body/4, trace_meta, on).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% auxiliaries
-
-exquant_body(_, Goal, Module) :-
-	untraced_call(Goal, Module).
-
-:- unskipped exquant_body/3.
+:- set_flag((^)/2, trace_meta, on).
 :- set_flag(exquant_body/3, trace_meta, on).
+
