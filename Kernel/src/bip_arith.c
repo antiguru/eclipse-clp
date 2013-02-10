2f3b5956abe7993ff3f9f5ccedb993cd70d268be
jschimpf <jschimpf>
2013-02-10 00:28:49 +0000

    Removed incorrectly merged-in lines

--- Kernel/src/bip_arith.c
+++ Kernel/src/bip_arith.c
@@ -21,7 +21,7 @@
  * END LICENSE BLOCK */
 
 /*
- * VERSION    $Id: bip_arith.c,v 1.20 2013/02/09 20:27:57 jschimpf Exp $
+ * VERSION    $Id: bip_arith.c,v 1.21 2013/02/10 00:28:49 jschimpf Exp $
  */
 
 /*
@@ -111,11 +111,6 @@
 
 static int
 	_reverse_times(word x, word y, value zval, type ztag);
-
-
-#if defined(i386) && defined(__GNUC__)
-double (*pow_ptr_to_avoid_buggy_inlining)(double,double) = pow;
-#endif
 
 
 /*------------------------------------------------------------------------
