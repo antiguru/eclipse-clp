26fe2284c4205694e7fe906c5f5c90848757e52e
kish_shen <kish_shen>
2016-03-14 21:33:43 +0000

    undo accidental check-in of debugging code

--- Oci/dbi.c
+++ Oci/dbi.c
@@ -26,7 +26,7 @@
  *
  * ECLiPSe LIBRARY MODULE
  *
- * $Header: /cvsroot/eclipse-clp/Eclipse/Oci/dbi.c,v 1.5 2016/03/14 21:27:23 kish_shen Exp $
+ * $Header: /cvsroot/eclipse-clp/Eclipse/Oci/dbi.c,v 1.6 2016/03/14 21:33:43 kish_shen Exp $
  *
  *
  * IDENTIFICATION:	dbi.c
@@ -296,7 +296,6 @@ void
 session_free(session_t * session)
 {
 
-printf("Freeing session`n");
     if (session == NULL) return;
 
     if (--(session->refs) == 0)
@@ -312,9 +311,9 @@ printf("Freeing session`n");
 	}
 	free(session);
     }
-/*#ifdef DEBUGA*/
+#ifdef DEBUG
     fprintf(stderr,"session free\n");
-/*#endif*/
+#endif
 
     return;
 }
@@ -832,15 +831,12 @@ p_cursor_free(value v_cursorh, type t_cursorh)
 {
     pword handle;
 
-    printf("Freeing cursor....\n");
     handle.val.all = v_cursorh.all;
     handle.tag.all = t_cursorh.all;
 
-/*    return ec_free_handle(handle, &cursor_handle_tid); */
-    {int res= ec_free_handle(handle, &cursor_handle_tid);
+    return ec_free_handle(handle, &cursor_handle_tid);
 
-    printf("Freed cursor...\n");
-    return res;}
+
 }
 
 int
